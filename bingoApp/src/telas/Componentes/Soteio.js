37ac0811c61771569a4c2081e4e189cafcc0b25b
Marcos Aragão <marcosvinisa@icloud.com>
2022-12-19 18:09:16 -0300

    v19122022

--- bingoApp/src/telas/Componentes/Soteio.js
+++ bingoApp/src/telas/Componentes/Soteio.js
@@ -2,30 +2,41 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 
+const numeroSorteio = [];
+
 export default function SorteiaNumero() {
 
     const geraSorteio = () => {
-        var numeroAleatorio = Math.random();
-        numeroAleatorio = Math.floor(numeroAleatorio * 99)
-        alert(numeroAleatorio);
-
+        let bola = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
+        let existeBola = numeroSorteio.find(item => item === bola);
+        if (existeBola) {
+            geraSorteio(); // Tenta novamente
+            return;
+        }
+        alert(bola);
+        numeroSorteio.push(bola);
+        console.log(numeroSorteio);
     }
 
+
     return <View>
-        <Text style={styles.numero}>0</Text>
+        <Text style={styles.numero}> Numeros ja sorteados: </Text>
+        {numeroSorteio.map(()=>{
+            <Text style={styles.numero} >{numeroSorteio}</Text>
+        })}
         <TouchableOpacity
             style={styles.botao}
             onPress={geraSorteio}>
             <Text style={styles.btnText}>Sortear</Text>
         </TouchableOpacity>
     </View>
+
 }
 const styles = StyleSheet.create({
-
     numero: {
         color: "#FFFFFF",
         textAlign: "center",
-        fontSize: "100",
+        fontSize: "20",
     },
     botao: {
         backgroundColor: "#14fa55",
