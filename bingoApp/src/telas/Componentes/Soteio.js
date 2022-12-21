2da323daea82569d90a1d6e871afd5e74c5e37bb
Marcos Aragão <marcosvinisa@icloud.com>
2022-12-21 15:53:47 -0300

    v21122022

--- bingoApp/src/telas/Componentes/Soteio.js
+++ bingoApp/src/telas/Componentes/Soteio.js
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-
- const numeroSorteio = [];
+const numeroSorteio = [];
 
 export default function SorteiaNumero() {
-   
-    const geraSorteio = () => {
-        let bola = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
-        let existeBola = numeroSorteio.find(item => item === bola);
-        if (existeBola) {
-            geraSorteio(); // Tenta novamente
-            return;
-        }
-        alert(bola);
-        numeroSorteio.push(bola);
-        console.log(numeroSorteio);
+
+    const [ultimaBola, setUltimaBola] = useState()
+
+    let bola = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    let existeBola = numeroSorteio.find(item => item === bola);
+    if (existeBola) {
+        Bingo(); // Tenta novamente
+        return;
     }
-    console.log(numeroSorteio)
-    
+    function Bingo() {
+        numeroSorteio.push(bola);
+        setUltimaBola(bola);
+    }
+
+    const filtered = numeroSorteio.filter((item, index) => numeroSorteio.indexOf(item) === index);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.numero}> Numeros ja sorteados: </Text>
-            {numeroSorteio.map((key, item) => {
-                <Text key={key} style={styles.numero}>{item}</Text>
-            })}
+            <Text style={styles.numero}> {ultimaBola} </Text>
             <TouchableOpacity
-                style={styles.botao}
-                onPress={geraSorteio}>
+                style={styles.botaoSorteio}
+                onPress={Bingo}
+            >
                 <Text style={styles.btnText}>Sortear</Text>
             </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.botaoResultado}
+                onPress={() => { alert(filtered) }}>
+                <Text style={styles.btnText}>Lista de Sorteados</Text>
+            </TouchableOpacity>
+
         </View>
     )
 }
@@ -40,8 +46,17 @@ const styles = StyleSheet.create({
         fontSize: "20",
         padding: 15
     },
-    botao: {
+    botaoSorteio: {
         backgroundColor: "#14fa55",
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 15,
+        borderRadius: 5,
+        marginHorizontal: 80
+    },
+    botaoResultado: {
+        backgroundColor: "#ffbabe",
         height: 60,
         justifyContent: 'center',
         alignItems: 'center',
