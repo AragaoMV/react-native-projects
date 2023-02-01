6a6078364128f7bf583ed2723bca865488e421ab
Marcos Aragão <marcosvinisa@icloud.com>
2023-02-01 10:50:25 -0300

    v01022023

--- salvaLocalApp/src/servicos/Notas.js
+++ salvaLocalApp/src/servicos/Notas.js
@@ -1,9 +1,32 @@
 import { db } from "./SQLite";
 
-export function criaTabela(){
-    db.transaction((transaction)=> {
+export function criaTabela() {
+    db.transaction((transaction) => {
         transaction.executeSql("CREATE TABLE IF NOT EXISTS " +
-            "Notas " + 
+            "Notas " +
             "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, categoria Text, texto TEXT);")
     })
+}
+
+export async function addNota() {
+    return new Promise((resolve) => {
+        db.transaction((transaction) => {
+            transaction.executeSql("INSERT INTO Notas (titulo, categoria, texto) VALUES (?,?,?);",
+                [notas.titulo, nota.categoria, nota.texto], () => {
+                    resolve("Nota adicionada com sucesso!")
+                })
+        })
+    })
+
+}
+
+export async function buscaNotas() {
+    return new Promise((resolve) => {
+        db.transaction((transaction) => {
+            transaction.executeSql("SELECT * FROM Notas;",[], (transaction, resultado) => {
+                    resolve(resultado.rows._array)
+                })
+        })
+    })
+
 }
